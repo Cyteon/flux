f8974e226ce111b979afc1c6d88c718eda43ce07
Cyteon <pkristian1@outlook.com>
2025-04-28 08:51:54 +0000

    No media found, also where tf is /api/media gone, i swear i made it on my pc. did git go kaboom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,29 @@
         newest: [],
     };
 
+    let isMediaFound: boolean = true;
+
     onMount(async () => {
-        const res = await fetch("/api/media/homepage", {
-            headers: {
-                "Authorization": `Bearer ${getCookie("token")}`,
-            },
-        });
-        
-        if (res.ok) {
-            movies = await res.json();
-        } else {
-            console.error("Failed to fetch movies");
+        try {
+            const res = await fetch("/api/media/homepage", {
+                headers: {
+                    "Authorization": `Bearer ${getCookie("token")}`,
+                },
+            });
+            
+            if (res.ok) {
+                movies = await res.json();
+
+                if (movies.recentlyAdded.length === 0) {
+                    isMediaFound = false;
+                }
+            } else {
+                console.error("Failed to fetch movies");
+                isMediaFound = false;
+            }
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+            isMediaFound = false;
         }
     })
 </script>
@@ -31,32 +43,38 @@
 <div class="flex flex-col h-screen">
     <Navbar />
 
-    <div class="p-1 px-2 md:p-4 md:px-8">
-        <div>
-            <h1 class="text-2xl">Recently Added</h1>
-            <div class="flex mt-1">
-                {#each movies.recentlyAdded as movie}
-                    <Poster {movie} />
-                {/each}
+    {#if isMediaFound}
+        <div class="p-1 px-2 md:p-4 md:px-8">
+            <div>
+                <h1 class="text-2xl">Recently Added</h1>
+                <div class="flex mt-1">
+                    {#each movies.recentlyAdded as movie}
+                        <Poster {movie} />
+                    {/each}
+                </div>
             </div>
-        </div>
 
-        <div class="mt-4">
-            <h1 class="text-2xl">Newest</h1>
-            <div class="flex mt-1">
-                {#each movies.newest as movie}
-                    <Poster {movie} />
-                {/each}
+            <div class="mt-4">
+                <h1 class="text-2xl">Newest</h1>
+                <div class="flex mt-1">
+                    {#each movies.newest as movie}
+                        <Poster {movie} />
+                    {/each}
+                </div>
             </div>
-        </div>
 
-        <div class="mt-4">
-            <h1 class="text-2xl">Top Rated</h1>
-            <div class="flex mt-1">
-                {#each movies.topRated as movie}
-                    <Poster {movie} />
-                {/each}
+            <div class="mt-4">
+                <h1 class="text-2xl">Top Rated</h1>
+                <div class="flex mt-1">
+                    {#each movies.topRated as movie}
+                        <Poster {movie} />
+                    {/each}
+                </div>
             </div>
         </div>
-    </div>
+    {:else}
+        <div class="flex items-center justify-center h-full">
+            <h1 class="text-2xl">No media found</h1>
+        </div>
+    {/if}
 </div>
